8588082cc3e01c055d2aa27cef4546244eaf4ce9
Melvin-Cordon <mevin.cordon21@gmail.com>
2020-07-19 22:07:35 -0500

    Added "insert_end" and "Insert_between" function to SlinkedList class

--- Singly and doubly linked lists/singly_linked_list.py
+++ Singly and doubly linked lists/singly_linked_list.py
@@ -10,20 +10,45 @@ class SlinkedList():
         self.head = None
 
     def printlist(self):
-        curr_head = self.head
-        while curr_head != None:
-            print(curr_head.data)
-            curr_head = curr_head.next
+        curr_node = self.head
+        while curr_node != None:
+            print(curr_node.data)
+            curr_node = curr_node.next
 
-    def insert_beginning(self,val):
+    def insert_beginning(self, val):
         self.head = Node(val, self.head)
+
+    def insert_between(self, middle_node, val):
+        if middle_node is None:
+            print("The mentioned node is absent")
+            return
+
+        middle_node.next = Node(val, middle_node.next)
+
+    def insert_end(self, val):
+        if self.head == None:
+            self.head = Node(val)
+
+        curr_node = self.head
+        while curr_node.next:
+            curr_node = curr_node.next
+        curr_node.next = Node(val, next = None)
+
 
 list = SlinkedList()
 list.head = Node('Monday')
 list.head.next = Node('Tuesday')
 list.printlist()
-
 print("\n")
 
 list.insert_beginning('Sunday')
 list.printlist()
+print("\n")
+
+list.insert_end('Wednesday')
+list.printlist()
+print("\n")
+
+list.insert_between(list.head.next,'6')
+list.printlist()
+print("\n")
