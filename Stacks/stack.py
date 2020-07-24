7df00789b7c1fad770f9d2f6c3a8972748b9457f
Melvin-Cordon <melvin.cordon21@gmail.com>
2020-07-24 11:32:22 -0500

    created a stack data structure with pop and push methods.

---
+++ Stacks/stack.py
@@ -0,0 +1,43 @@
+""" Linked List Data Structure in Python """
+
+class Node():
+    def __init__(self,data = None, next = None):
+        self.data = data
+        self.next = next
+
+class stack():
+    def __init__(self):
+        self.head = None
+
+    def printlist(self):
+        curr_node = self.head
+        while curr_node != None:
+            print(curr_node.data)
+            curr_node = curr_node.next
+
+    def push(self, val):
+        self.head = Node(val, self.head)
+
+    def pop(self):
+        curr_node = self.head
+        self.head = curr_node.next
+        curr_node = None
+
+        return
+
+train = stack()
+train.push(1)
+train.push(2)
+train.push(3)
+train.push(4)
+train.printlist()
+print("\n")
+
+train.pop()
+train.printlist()
+print("\n")
+
+train.pop()
+train.pop()
+train.printlist()
+print("\n")
